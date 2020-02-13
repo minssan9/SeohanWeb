7e308ffd9b4a6cf1371252bf654c85c0c9043cc3
minssan9 <minssan9@gmail.com>
2020-02-13 16:13:22 +0900

    SeohanWeb -> Web, vuejs 분리

--- vueServer/src/services/auth/auth-header.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader () {
-    let accessToken = localStorage.getItem('accessToken');
-    if (accessToken) {
-      return { Authorization: accessToken }
-    } else {
-      return false
-    }
-  }
