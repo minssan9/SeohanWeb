4107feeb56d178ab7fe756b3daabbc109bf67f22
민상훈 <minssan9@gmail.com>
2020-06-18 21:30:56 +0900

    PoDto 제거

--- src/main/java/com/seohan/fta/Mapper/PoHeaderRepository.java
+++ src/main/java/com/seohan/fta/Mapper/PoHeaderRepository.java
@@ -1,17 +1,12 @@
 package com.seohan.fta.Mapper;
 
-import com.seohan.fta.Domain.PoDto;
 import com.seohan.fta.Domain.PoHeader;
-import com.seohan.fta.Domain.PoHeaderId;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface PoHeaderRepository extends JpaRepository<PoHeader, Long> {
@@ -26,7 +21,5 @@ public interface PoHeaderRepository extends JpaRepository<PoHeader, Long> {
     @Query(value = "select a, b from PoHeader a " +
             " left join com.seohan.fta.Domain.PoDetail b on a.cogb=b.cogb and a.pono=b.pono " +
             " where a.cogb=:cogb and a.poYmd=:poYmd")
-    List<PoDto> findByCogbAndPoYmd(String cogb, String poYmd);
-
-
+    List<PoHeader> findByCogbAndPoYmd(String cogb, String poYmd);
 }
