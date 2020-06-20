d31a65f4fb94e32629d0632af460395cf6e1f921
민상훈 <minssan9@gmail.com>
2020-06-20 14:09:13 +0900

    balance 08h save jdbcTemplate batchUpdate

---
+++ src/main/java/com/seohan/mat/Mapper/ItemBalanceHisRepository.java
@@ -0,0 +1,16 @@
+package com.seohan.mat.Mapper;
+
+import com.seohan.mat.Domain.ItemBalance;
+import com.seohan.mat.Domain.ItemBalanceHis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemBalanceHisRepository extends JpaRepository<ItemBalanceHis, Long> {
+	ItemBalanceHis findByGdateAndGtimeAndWarhsAndItmno(String gdate, String gtime, String warhs, String itmno);
+
+	List<ItemBalanceHis> findByGdateAndGtime(String gdate, String gtime);
+	List<ItemBalanceHis> save(List<ItemBalanceHis> itemBalanceHis);
+}
