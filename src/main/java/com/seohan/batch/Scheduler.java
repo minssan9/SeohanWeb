c2fc9f3146f61ec797c2c1258772fa6251b6bf30
Min Mike <minssan9@gmail.com>
2020-07-07 14:16:32 +0900

    pom.xml - spring batch - version - test Scheduler config - balance save job added

--- src/main/java/com/seohan/batch/Scheduler.java
+++ src/main/java/com/seohan/batch/Scheduler.java
@@ -1,10 +1,7 @@
-package com.seohan.scheduler;
+package com.seohan.batch;
 
-import com.seohan.mat.Mapper.ItemBalanceHisRepository;
-import com.seohan.mat.Mapper.ItemBalanceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,24 +11,22 @@ import java.util.Date;
 @Slf4j
 @Component
 public class Scheduler  {
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
+
 	@Autowired
 	private ScheduledJobs scheduledJobs;
 
 	@Scheduled(cron = "0 0 8 * * ?")
 	public void saveBalanceJobSch() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
-		Date now = new Date();
-		String strDate = sdf.format(now);
-
+		String strDate = sdf.format(new Date());
 		scheduledJobs.saveBalance(strDate );
 		System.out.println("Java cron job expression:: " + strDate);
 	}
 
 	@Scheduled(fixedDelay = 1000)
 	public void getStockDataSch() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		Date now = new Date();
-		String strDate = sdf.format(now);
+		String strDate = sdf.format(new Date());
+		scheduledJobs.saveBalance(strDate );
 //		System.out.println("Java cron job expression:: " + strDate);
 	}
 }
