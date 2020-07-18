6a69e181d4ab35012b347267fff901e4a0c3f3cd
민상훈 <minssan9@gmail.com>
2020-07-18 21:50:07 +0900

    queryDsl 제거

---
+++ src/main/java/com/seohan/Config/Const.java
@@ -0,0 +1,6 @@
+package com.seohan.Config;
+
+public class Const {
+    public static final String CONFIG_FILE_PATH = "config/server.config.json";
+    public static final String DEFAULT_SERVER_NAME = "localhost";
+}
