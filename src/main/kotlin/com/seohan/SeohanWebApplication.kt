2a96ef3c900a553dc6abf99aeff56e3c3b60fff4
민상훈 <minssan9@gmail.com>
2020-07-29 20:51:58 +0900

    import runApplication

--- src/main/kotlin/com/seohan/SeohanWebApplication.kt
+++ src/main/kotlin/com/seohan/SeohanWebApplication.kt
@@ -1,14 +1,12 @@
 package com.seohan
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.context.annotation .Configuration
-import org.springframework.context.annotation .PropertySource
-import org.springframework.scheduling.annotation .EnableScheduling
-import org.springframework.web.bind.annotation .CrossOrigin
+import org.springframework.context.annotation.PropertySource
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @SpringBootApplication
 @EnableScheduling
