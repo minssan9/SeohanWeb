561931507c109142fef6dd1469ad03053ede6cfd
민상훈 <minssan9@gmail.com>
2020-07-29 20:26:12 +0900

    application.kt integration Test

--- src/main/java/com/seohan/SeohanWebApplication.java
+++ src/main/java/com/seohan/SeohanWebApplication.java
@@ -1,28 +1,28 @@
-package com.seohan;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@SpringBootApplication
-@EnableScheduling
-//@Configuration
-@PropertySource(value = {"classpath:account.properties" })
-@CrossOrigin(origins = { "http://localhost:8091",  "http://localhost:8090",  "http://localhost"})
-public class SeohanWebApplication extends SpringBootServletInitializer {
- 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SeohanWebApplication.class);
-	}
-  
-	public static void main(String[] args) {
-		SpringApplication.run(SeohanWebApplication.class, args);
-	}
-}
+//package com.seohan;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//
+//@SpringBootApplication
+//@EnableScheduling
+////@Configuration
+//@PropertySource(value = {"classpath:account.properties" })
+//@CrossOrigin(origins = { "http://localhost:8091",  "http://localhost:8090",  "http://localhost"})
+//public class SeohanWebApplication extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(SeohanWebApplication.class);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SeohanWebApplication.class, args);
+//	}
+//}
---
+++ src/main/kotlin/com/seohan/SeohanWebApplication.kt
@@ -0,0 +1,36 @@
+package com.seohan
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.annotation.PropertySource
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.bind.annotation.CrossOrigin
+
+package com.seohan
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.annotation .Configuration
+import org.springframework.context.annotation .PropertySource
+import org.springframework.scheduling.annotation .EnableScheduling
+import org.springframework.web.bind.annotation .CrossOrigin
+
+@SpringBootApplication
+@EnableScheduling
+//@Configuration
+@PropertySource(value = ["classpath:account.properties"])
+@CrossOrigin(origins = ["http://localhost:8091", "http://localhost:8090", "http://localhost"])
+open class SeohanWebApplication : SpringBootServletInitializer() {
+
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(SeohanWebApplication::class.java!!)
+    }
+    fun main(args: Array<String>) {
+        runApplication<SeohanWebApplication>(*args)
+    }
+}
