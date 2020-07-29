f67b1e11674d697e1c35e449e00eb72921046dab
민상훈 <minssan9@gmail.com>
2020-07-29 20:48:25 +0900

    project tree 변경 /java  -> /kotlin pom.xml kotlin-version 1.3.71 변경

--- src/main/kotlin/com/seohan/SeohanWebApplication.kt
+++ src/main/kotlin/com/seohan/SeohanWebApplication.kt
@@ -1,16 +1,6 @@
 package com.seohan
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.context.annotation.PropertySource
-import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.bind.annotation.CrossOrigin
-
-package com.seohan
-
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
