7b0ab643d5774e39e898b42cd087de70d24851fb
Min Mike <minssan9@gmail.com>
2020-08-28 11:48:14 +0900

    Loca Alm saveAll

--- src/main/java/com/seohan/erp/lab/Controller/LocaAlmRestController.java
+++ src/main/java/com/seohan/erp/lab/Controller/LocaAlmRestController.java
@@ -2,16 +2,14 @@ package com.seohan.erp.lab.Controller;
 
 import com.seohan.erp.lab.Domain.LocaAlmEntity;
 import com.seohan.erp.lab.Mapper.LocaAlmEntityRepository;
+import com.seohan.erp.lab.Service.LocaAlmEntityService;
 import com.seohan.erp.mat.Dto.ImportPlanAlarm;
 import com.seohan.erp.mat.Service.ImportPlanService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -21,10 +19,17 @@ import java.util.List;
 @RestController
 class LocaAlmRestController {
 	@Autowired
+	private LocaAlmEntityService locaAlmEntityService;
+	@Autowired
 	private LocaAlmEntityRepository locaAlmEntityRepository;
 
 	@GetMapping
 	public ResponseEntity<List<LocaAlmEntity>> getLocaAlmList() {
 		return new ResponseEntity<List<LocaAlmEntity>>(locaAlmEntityRepository.findLocaAlmEntityByGubnAndSts("A2","1"), HttpStatus.OK);
+	}
+
+	@PutMapping
+	public ResponseEntity<List<LocaAlmEntity>> endLocaAlmEntity(List<LocaAlmEntity> locaAlmEntitys) {
+		return new ResponseEntity<List<LocaAlmEntity>>(locaAlmEntityService.endLocaAlmEntity(locaAlmEntitys), HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/seohan/erp/lab/Service/LocaAlmEntityService.java
@@ -0,0 +1,10 @@
+package com.seohan.erp.lab.Service;
+
+import com.seohan.erp.general.Domain.FoodTable;
+import com.seohan.erp.lab.Domain.LocaAlmEntity;
+
+import java.util.List;
+
+public interface LocaAlmEntityService {
+	public List<LocaAlmEntity> endLocaAlmEntity(List<LocaAlmEntity> locaAlmEntitys) ;
+}
