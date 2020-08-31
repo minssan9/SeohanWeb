d7c815fd67f62ee79ee1228577b21f3937139a97
Min Mike <minssan9@gmail.com>
2020-08-31 11:35:01 +0900

    온습도계 업로드

--- src/main/java/com/seohan/dev/temp/Domain/TemperhdEntity.java
+++ src/main/java/com/seohan/dev/temp/Domain/TemperhdEntity.java
@@ -14,16 +14,26 @@ public class TemperhdEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "CO_GB")
     private String coGb;
     private String warhs;
     private String line;
+
+    @Column(name = "IP_ADDRESS")
     private String ipAddress;
+    @Column(name = "T_CYCLE")
     private BigDecimal tCycle;
+    @Column(name = "TEMPER_MIN")
     private BigDecimal temperMin;
+    @Column(name = "TEMPER_MAX")
     private BigDecimal temperMax;
+    @Column(name = "HUMID_MIN")
     private BigDecimal humidMin;
+    @Column(name = "HUMID_MAX")
     private BigDecimal humidMax;
     private String state;
     private String bigo;
+    @Column(name = "CRT_DATE")
     private String crtDate;
 }
