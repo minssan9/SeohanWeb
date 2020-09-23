9d6be4e46724b19bfe52bf44ed067dc2c9bfdbbd
Min Mike <minssan9@gmail.com>
2020-09-23 12:04:25 +0900

    /dev 인증 제외

--- src/main/java/com/seohan/config/ResourceConfig.java
+++ src/main/java/com/seohan/config/ResourceConfig.java
@@ -29,8 +29,8 @@ ResourceConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/websockethandler/**").permitAll()
                 .antMatchers("/app/**").permitAll()
-                .mvcMatchers(HttpMethod.GET, "/api/**").permitAll()  // 해당 요청은 누구나 가능하며
-                .mvcMatchers(HttpMethod.POST, "/dev").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/**").permitAll()  // 해당 요청은 누구나 가능하며
+                .mvcMatchers(HttpMethod.POST, "/dev/**").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/oauth").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/auth/join/check").permitAll()
                 .anyRequest().authenticated()       //  나머지 요청은 권한이 필요합니다.
